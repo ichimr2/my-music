9da5f5aa058bae1689442d35c16e45ff791e405a
Robert-Ionut Ichim <ichimr2@coventry.ac.uk>
2020-12-15 16:46:59 +0000

    retrieve metadata from the database

---
+++ modules/metadata.js
@@ -0,0 +1,43 @@
+
+/** @module Metadata */
+
+import sqlite from 'sqlite-async'
+
+
+/**
+ * Metadata
+ * ES6 module that manages the metadata in the system.
+ */
+class Metadata {
+	/**
+   * Create an account object
+   * @param {String} [dbName=":memory:"] - The name of the database file to use.
+   */
+	constructor(dbName = ':memory:') {
+		return (async() => {
+			this.db = await sqlite.open(dbName)
+			// we need this table to store the user accounts
+			const sql = 'CREATE TABLE IF NOT EXISTS trackinfo(\
+					id INTEGER PRIMARY KEY AUTOINCREMENT,\
+					userid INTEGER,\
+					username TEXT NOT NULL,\
+					trackname TEXT,\
+					artistname TEXT,\
+					albumart TEXT,\
+					duration TEXT,\
+					FOREIGN KEY(userid) REFERENCES users(id)\
+				);'
+			await this.db.run(sql)
+			return this
+		})()
+	}
+
+	async all() {
+		const sql = 'SELECT users.user, trackinfo.* FROM trackinfo, users\
+						WHERE trackinfo.id = users.id;'
+		const metadata = await this.db.all(sql)
+		return metadata
+	}
+}
+
+export default Metadata
--- routes/secure.js
+++ routes/secure.js
@@ -1,12 +1,19 @@
-
 import Router from 'koa-router'
+import bodyParser from 'koa-body'
 import mime from 'mime-types'
 import fs from 'fs-extra'
+import Koa from 'koa'
 import serve from 'koa-static'
+import views from 'koa-views'
 import * as mm from 'music-metadata';
 import * as util from 'util'
-import views from 'koa-views'
+import Accounts from '../modules/accounts.js'
+import Music from '../modules/music.js'
+import Metadata from '../../5009CEM/modules/metadata.js'
+const dbName = 'website.db'
 const router = new Router({ prefix: '/secure' })
+router.use(bodyParser({multipart: true}))
+
 
 async function checkAuth(ctx, next) {
 	console.log('secure router middleware')
@@ -18,65 +25,23 @@ async function checkAuth(ctx, next) {
 router.use(checkAuth)
 
 router.get('/', async ctx => {
+	const metadata = await new Metadata(dbName)
 	try {
+		console.log('ss')
+		const records = await metadata.all()
+		console.log(records)
+		ctx.hbs.records = records
 		await ctx.render('secure', ctx.hbs)
 	} catch(err) {
+		console.log('AI BELUT PUALFDSAFAS')
 		ctx.hbs.error = err.message
 		await ctx.render('error', ctx.hbs)
+		console.log('finally')
 	}
 })
 
-router.get('/upload', async ctx => {
-	try {
- 
-		await ctx.render('upload', ctx.hbs)
-	} catch(err) {
-		ctx.hbs.error = err.message
-		await ctx.render('error', ctx.hbs)
-	}
-})
 
-router.post('/upload', async ctx => {
-	try {
-			console.log(ctx.session.user)
- 
-		var myfile = ctx.request.files.myfile
-		myfile.extension = mime.extension(myfile.type)
-		if (myfile.type != "audio/mpeg")
-			{
-			throw new Error('wrong file type');
-			}
- 
-    await fs.copy(myfile.path, `uploads/${myfile.name}`) 	
-	}
- 
-	catch(err) {
- 		 await ctx.render('index')
- 		 console.log('first catch')
-     console.log(err.message)
-  }
- 
-	var musicData = {}
- 
-	const parsedFile = await mm.parseFile(`uploads/${myfile.name}`)
-	 let duration = parsedFile.format['duration']
-	 console.log(duration)
-	 let title = parsedFile.common['title']
-	 let album = parsedFile.common['album']
-	 let artist = parsedFile.common['artist']
-	 musicData.duration = duration
-	  musicData.title = title
-	 musicData.album = album
-	 musicData.artist = artist
-	ctx.hbs.data = musicData
-	console.log(ctx.hbs)
-	await ctx.render('secure', ctx.hbs)
- 
- 
- 
- 
- 
- 
-});
+
+
 
 export default router
